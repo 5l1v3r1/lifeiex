fb05b17d68f0285ee8916c022e0c1655e31719e8
fabienngo <fabien.ngo@keatext.com>
2018-08-26 21:42:51 -0400

    fix test after privatizing some LifeCore functions

--- test/life_test.exs
+++ test/life_test.exs
@@ -1,9 +1,30 @@
 defmodule LifeTest do
   use ExUnit.Case
-  doctest Life
+  doctest LifeCore
 
-  test "compute_neighboors" do
-    assert Life.compute_neighboors(0, 0) ==
-             MapSet.new([{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}])
+  test "next_iter" do
+    s = MapSet.new([{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}])
+
+    expected =
+      MapSet.new([
+        {-2, -1},
+        {-2, 0},
+        {-2, 1},
+        {-1, -2},
+        {-1, -1},
+        {-1, 1},
+        {-1, 2},
+        {0, -2},
+        {0, 2},
+        {1, -2},
+        {1, -1},
+        {1, 1},
+        {1, 2},
+        {2, -1},
+        {2, 0},
+        {2, 1}
+      ])
+
+    assert LifeCore.next_iter(s) == expected
   end
 end
